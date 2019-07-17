968f1e017e54cf7bc5ad83b07f252b54582d1a2b
osmarpetry <osmarpetry@gmail.com>
2019-07-16 21:51:20 -0300

    minimal visual setup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import logo from './spotify-logo.svg'
 
 const App = () => (
     <div className='App'>
         <header className='App-header'>
-            <img src={ logo } className='App-logo' alt='logo' />
-            <p>
-                    Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-                className='App-link'
-                href='https://reactjs.org'
-                target='_blank'
-                rel='noopener noreferrer'>
-                    Learn React
-            </a>
+            <img
+                src={ logo }
+                name='App-logo'
+                alt='logo'
+                width='200'
+                height='100'
+            />
+            Black Spotify
         </header>
     </div>
 )
