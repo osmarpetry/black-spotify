82bf87c49ea13f81eb1332f09b5b0f654deeaeba
osmarpetry <osmarpetry@gmail.com>
2019-07-17 23:38:29 -0300

    moved to hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import logo from './spotify-logo.svg'
 import queryString from 'query-string'
 
@@ -6,43 +6,26 @@ import Album from './components/Album'
 import Banner from './components/Banner'
 import Search from './components/Search'
 
-class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            serverData: {},
-            searchText: '',
-            selectedId: ''
-        }
+const App = () => {
+    const accessToken = queryString.parse(window.location.search).access_token
+
+    const [serverData, setServerData] = useState({})
+    const [searchText, setSearchText] = useState('')
+    const [selectedId, setSelectedId] = useState('')
+
+    const handleSelectedId = selectedId => () => {
+        setSelectedId(selectedId)
     }
 
-    handleSelectedId = selectedId => () => {
-        this.setState({ selectedId })
+    const handelSearch = event => {
+        setSearchText(event.target.value)
     }
 
-    handelSearch = event => {
-        this.setState({ searchText: event.target.value })
-    }
-
-    handleLogin = () => {
+    const handleLogin = () => {
         window.location = 'https://black-spotify.herokuapp.com/login'
     }
 
-    renderAlbum = (src, alt, tracks) => {
-        return (
-            <div>
-                <Banner
-                    src={ src }
-                    alt={ alt }
-                />
-                <Album tracks={ tracks } />
-            </div>
-        )
-    }
-
-    componentDidMount() {
-        const accessToken = queryString.parse(window.location.search).access_token
-
+    useEffect(() => {
         fetch(
             'https://api.spotify.com/v1/recommendations?limit=10&market=BR' +
             '&seed_artists=4NHQUGzhtTLFvgF5SZesLK&seed_genres=classical%2' +
@@ -50,130 +33,114 @@ class App extends Component {
                 headers: { 'Authorization': 'Bearer ' + accessToken }
             })
             .then(res => res.json())
-            .then(data => this.setState({
-                serverData: {
+            .then(data => {
+                setServerData({
                     albums: data.tracks
-                }
-            }))
-            .catch(console.log)
-    }
+                })
+            })
+    }, [])
 
-    componentDidUpdate(prevProps, prevState) {
-        const accessToken = queryString.parse(window.location.search).access_token
-
-        if (
-            this.state.selectedId !== '' &&
-            prevState.selectedId !== this.state.selectedId
-        ) {
-            console.log('entrei: ', this.state.selectedId)
+    useEffect(() => {
+        if(selectedId !== '') {
             fetch(
                 'https://api.spotify.com/v1/albums/' +
-                this.state.selectedId, {
+            selectedId, {
                     headers: { 'Authorization': 'Bearer ' + accessToken }
                 })
                 .then(res => res.json())
                 .then(data => {
-                    console.log(data)
-                    this.setState({
-                        serverData: {
-                            album: data
-                        }
+                    setServerData({
+                        album: data
                     })
                 })
         }
+    }, [selectedId])
 
-        if (
-            this.state.searchText !== '' &&
-            prevState.searchText !== this.state.searchText
-        ) {
+    useEffect(() => {
+        if(searchText !== '') {
             fetch(
                 'https://api.spotify.com/v1/search?q=' +
-                this.state.searchText +
-                '&type=album&market=US&limit=10&offset=10', {
+            searchText +
+            '&type=album&market=US&limit=10&offset=10', {
                     headers: { 'Authorization': 'Bearer ' + accessToken }
                 })
                 .then(res => res.json())
-                .then(data => this.setState({
-                    serverData: {
-                        albums: data.albums.items
-                    }
+                .then(data => setServerData({
+                    albums: data.albums.items
                 }))
         }
+    }, [searchText])
 
-    }
-
-    render() {
-        return (
-            <div className='App'>
-                <header className='App-header'>
-                    <a href={ window.location.href }>
-                        <img
-                            src={ logo }
-                            name='App-logo'
-                            alt='logo'
-                            width='200'
-                            height='100'
-                        />
-                    </a>
-                    Black Spotify
-                </header>
-                <Search
-                    onSearch={ this.handelSearch }
-                    searchText={ this.state.searchText }
-                />
-                <section
-                    style={ {
-                        display: 'flex',
-                        flexWrap: 'wrap',
-                        justifyContent: 'center'
-                    } }>
-                    { this.state.selectedId === ''
-                        ? this.state.serverData.albums ? (
-                            this.state.serverData.albums.map(res => {
-                                if (res.album) {
-                                    return (
-                                        <Banner
-                                            src={ res.album.images[0].url }
-                                            alt={ res.album.name }
-                                            key={ res.album.id }
-                                            onClick={
-                                                this.handleSelectedId(res.album.id)
-                                            }
-                                        />
-                                    )
-                                } else {
-                                    return (
-                                        <Banner
-                                            src={ res.images[0].url }
-                                            alt={ res.name }
-                                            key={ res.id }
-                                            onClick={ this.handleSelectedId(res.id) }
-                                        />
-                                    )
-                                }
-                            }
-                            )) : <button onClick={ this.handleLogin }>Login</button>
-                        : (
-                            this.state.serverData.album && (
-                                <div>
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <a href={ window.location.href }>
+                    <img
+                        src={ logo }
+                        name='App-logo'
+                        alt='logo'
+                        width='200'
+                        height='100'
+                    />
+                </a>
+                Black Spotify
+            </header>
+            <Search
+                onSearch={ handelSearch }
+                searchText={ searchText }
+            />
+            <section
+                style={ {
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    justifyContent: 'center'
+                } }>
+                { selectedId === ''
+                    ? serverData.albums ? (
+                        serverData.albums.map(res => {
+                            if (res.album) {
+                                return (
                                     <Banner
-                                        src={
-                                            this.state.serverData.album.images[0].url
-                                        }
-                                        alt={ this.state.serverData.album.name }
-                                    />
-                                    <Album
-                                        tracks={
-                                            this.state.serverData.album.tracks.items
+                                        src={ res.album.images[0].url }
+                                        alt={ res.album.name }
+                                        key={ res.album.id }
+                                        onClick={
+                                            handleSelectedId(res.album.id)
                                         }
                                     />
-                                </div>
-                            )
-                        ) }
-                </section>
-            </div>
-        )
-    }
+                                )
+                            } else {
+                                return (
+                                    <Banner
+                                        src={ res.images[0].url }
+                                        alt={ res.name }
+                                        key={ res.id }
+                                        onClick={ handleSelectedId(res.id) }
+                                    />
+                                )
+                            }
+                        }
+                        )) : <button onClick={ handleLogin }>Login</button>
+                    : (
+                        serverData.album && (
+                            <div>
+                                <Banner
+                                    src={
+                                        serverData.album.images[0].url
+                                    }
+                                    alt={ serverData.album.name }
+                                />
+                                <Album
+                                    tracks={
+                                        serverData.album.tracks.items
+                                    }
+                                />
+                            </div>
+                        )
+                    ) }
+            </section>
+        </div>
+    )
 }
 
 export default App
